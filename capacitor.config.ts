65e48e17adcff8b2dd23fb621ad503719509d6f5
PENA98 <maynorx5000@gmail.com>
2022-04-02 00:27:07 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Chronos',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
